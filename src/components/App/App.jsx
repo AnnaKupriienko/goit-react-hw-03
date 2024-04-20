b1723066279c4021e2d06cd3d8712859d79a7489
Anna <kupriienko16@gmail.com>
2024-04-20 21:31:49 +0100

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,19 @@
-import userData from "../../userData.json"
-import friends from "../../friends.json"
-import transactions from "../../ transactions.json"
-import Profile from "../Profile/Profile";
-import FriendList from "../Friendlist/FriendList"
-import TransactionHistory from "../TransactionHistory/TransactionHistory";
+// import contactList from "../../contactList.json"
 
-const App = () => {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions}/>
-    </>
-  );
-};
-export default App;
+// import SearchBox from '../SearchBox';
+import ContactList from '../ContactList/ContactList'
+
+export default function App() {
+    const contacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+    return (
+        <div>
+            <h1>Phonebook</h1>
+            <ContactList contacts={contacts} />
+        </div>
+    );
+}
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact({ name, number }) {
+    return (
+        <li>
+            <p>Name: {name}</p>
+            <p>Number: {number}</p>
+        </li>
+    );
+}
